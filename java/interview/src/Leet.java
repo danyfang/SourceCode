881c2d8671895566e7ebf89ccd946c764dbf9496
Xuqiang Fang <danyfang7@gmail.com>
2020-09-04 00:47:49 +0800

    update

--- java/interview/src/Leet.java
+++ java/interview/src/Leet.java
@@ -1757,7 +1757,7 @@ public class Leet {
         } else {
             odd[0] = 1;
         }
-        long MOD = 10^9 + 7;
+        long MOD = 1000000000 + 7;
         long ans = odd[0];
         for (int i=1; i<n; ++i) {
             if (arr[i] % 2 == 0) {
@@ -1769,6 +1769,7 @@ public class Leet {
             }
             ans += odd[i];
         }
+        ans %= MOD;
         return (int)ans;
     }
 
@@ -1940,5 +1941,118 @@ public class Leet {
             }
         }
         return uf.getParentCount();
+    }
+
+    public int removeCoveredIntervals(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+        int n = intervals.length;
+        Arrays.sort(intervals, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o2[1] - o1[1];
+            }
+            return o1[0] - o2[0];
+        });
+        int l = intervals[0][0];
+        int r = intervals[0][1];
+        int ans = 0;
+        for (int i=1; i<n; ++i) {
+            if (intervals[i][0] >= l && intervals[i][1] <= r) {
+                ans++;
+            } else {
+                l = intervals[i][0];
+                r = intervals[i][1];
+            }
+        }
+        return n - ans;
+    }
+
+    class CustomStack {
+        private int[] nums;
+        int size;
+        public CustomStack(int maxSize) {
+            nums = new int[maxSize];
+            size = -1;
+        }
+
+        public void push(int x) {
+            if (++size < nums.length) {
+                nums[size] = x;
+            } else {
+                size--;
+            }
+        }
+
+        public int pop() {
+            if (size >= 0) {
+                return nums[size--];
+            }
+            return -1;
+        }
+
+        public void increment(int k, int val) {
+            for (int i=0; i<=Math.min(k-1, size); ++i) {
+                nums[i] += val;
+            }
+        }
+    }
+
+    public List<String> printVertically(String s) {
+        List<String> ans =  new ArrayList<>();
+        int n = 0;
+        String[] arr = s.split(" ");
+        for (int i=0; i<arr.length; ++i) {
+            n = Math.max(n, arr[i].length());
+        }
+        for (int i=0; i<n; ++i) {
+            StringBuilder sb = new StringBuilder();
+            for (int j=0; j<arr.length; ++j) {
+                if (i >= arr[j].length()) {
+                    sb.append(' ');
+                } else {
+                    sb.append(arr[j].charAt(i));
+                }
+            }
+            while(sb.charAt(sb.length()-1) == ' ') {
+                sb.deleteCharAt(sb.length()-1);
+            }
+            ans.add(sb.toString());
+        }
+        return ans;
+    }
+
+    public String removeDuplicates(String s, int k) {
+        int current = 0;
+        Stack<Character> stack = new Stack<>();
+        Stack<Integer> size = new Stack<>();
+        for (int i=0; i<s.length(); ++i) {
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+                current = 1;
+            } else {
+                if (stack.peek() == s.charAt(i)) {
+                    stack.push(s.charAt(i));
+                    current++;
+                } else {
+                    stack.push(s.charAt(i));
+                    size.push(current);
+                    current = 1;
+                }
+            }
+            if (current == k) {
+                while (current-- > 0) {
+                    stack.pop();
+                }
+                if (!size.isEmpty()) {
+                    current = size.pop();
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
     }
 }
