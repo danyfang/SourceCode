3ca08dc5d142cbb3561a199382b5ca93352e93bf
Xuqiang Fang <xuqfang@microsoft.com>
2021-01-16 18:19:30 +0800

    update

---
+++ java/interview/src/network/Server.java
@@ -0,0 +1,64 @@
+package network;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(8390); // 监听指定端口
+        ServerSocket ss2 = new ServerSocket(8391); // 监听指定端口
+        System.out.println("server is running...");
+        for (;;) {
+            Socket sock = ss.accept();
+            System.out.println("connected from " + sock.getRemoteSocketAddress());
+            Thread t = new Handler(sock);
+            t.start();
+            Socket sock2 = ss2.accept();
+            System.out.println("connected from " + sock2.getRemoteSocketAddress());
+            Thread t2 = new Handler(sock2);
+            t2.start();
+        }
+    }
+}
+
+class Handler extends Thread {
+    Socket sock;
+
+    public Handler(Socket sock) {
+        this.sock = sock;
+    }
+
+    @Override
+    public void run() {
+        try (InputStream input = this.sock.getInputStream()) {
+            try (OutputStream output = this.sock.getOutputStream()) {
+                handle(input, output);
+            }
+        } catch (Exception e) {
+            try {
+                this.sock.close();
+            } catch (IOException ioe) {
+            }
+            System.out.println("client disconnected.");
+        }
+    }
+
+    private void handle(InputStream input, OutputStream output) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        writer.write("hello\n");
+        writer.flush();
+        for (;;) {
+            String s = reader.readLine();
+            if (s.equals("bye")) {
+                writer.write("bye\n");
+                writer.flush();
+                break;
+            }
+            writer.write("ok: " + s + "\n");
+            writer.flush();
+        }
+    }
+}
